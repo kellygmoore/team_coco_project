18e5e0f4ec837d13b42fdf187fde968557e511df
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-12-11 16:50:48 -0600

    starting comparing hours

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -19,9 +19,35 @@ myApp.controller('TimeCtrl', ["$scope", "$timeout", function($scope, $timeout) {
 
 //Controller for the CALENDAR ////////////////////////////////////////////////////////
 myApp.controller('CalendarCtrl', ["$scope", "$location", function($scope, $location){
+    //dummy data for time hours that room can be booked//
     $scope.bambooDataArray = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6];
+    //placeholder for who is in the room for that booked time//
     $scope.memberInRoom = "Santa Claus";
-    $scope.bookedTime = false;
+
+   //$scope.pastTime = false;
+   // var rightNow, booked, middle;
+   // var d = new Date();
+   // var c = new Date();
+   // //console.log(d + " " + c);
+   //
+   // rightNow = d.getHours();
+   // //console.log("Rightnow: ", rightNow);
+   // middle = c.setHours(9);
+   // var next = new Date(middle);
+   // //console.log("middle: ", next);
+   // booked = next.getHours();
+   // //booked = middle.getHours();
+   // //console.log("booked: ", booked);
+   //
+   // console.log("right now: ", rightNow + "booked: ", booked);
+   //
+   // if (booked < rightNow) {
+   //     $scope.pastTime = true;
+   // } else {
+   //     $scope.pastTime = false;
+   // }
+
+
 
 
 
