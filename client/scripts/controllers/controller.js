ad5ea571be05af1fc521ee43737fbf6814af8685
lahusiak <lahusiak@gmail.com>
2015-12-14 10:16:05 -0600

    Cleaned up modal example

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -29,38 +29,10 @@ myApp.controller('TimeCtrl', ["$scope", "$timeout", function($scope, $timeout) {
 myApp.controller('AppCtrl', ['$scope', '$mdDialog', '$mdMedia', function($scope, $mdDialog, $mdMedia){
 
 $scope.status = '  ';
+
 $scope.customFullscreen = $mdMedia('sm');
-//$scope.showAlert = function(ev) {
-//    // Appending dialog to document.body to cover sidenav in docs app
-//    // Modal dialogs should fully cover application
-//    // to prevent interaction outside of dialog
-//    $mdDialog.show(
-//        $mdDialog.alert()
-//            .parent(angular.element(document.querySelector('#popupContainer')))
-//            .clickOutsideToClose(true)
-//            .title('This is an alert title')
-//            .textContent('You can specify some description text in here.')
-//            .ariaLabel('Alert Dialog Demo')
-//            .ok('Got it!')
-//            .targetEvent(ev)
-//    );
-//};
-//$scope.showConfirm = function(ev) {
-//    // Appending dialog to document.body to cover sidenav in docs app
-//    var confirm = $mdDialog.confirm()
-//        .title('Would you like to delete your debt?')
-//        .textContent('All of the banks have agreed to forgive you your debts.')
-//        .ariaLabel('Lucky day')
-//        .targetEvent(ev)
-//        .ok('Please do it!')
-//        .cancel('Sounds like a scam');
-//    $mdDialog.show(confirm).then(function() {
-//        $scope.status = 'You decided to get rid of your debt.';
-//    }, function() {
-//        $scope.status = 'You decided to keep your debt.';
-//    });
-//};
-$scope.showAdvanced = function(ev) {
+
+$scope.showLogin = function(ev) {
     console.log("show advanced");
     $mdDialog.show({
         controller: DialogController,
@@ -81,20 +53,20 @@ $scope.showAdvanced = function(ev) {
         $scope.customFullscreen = (sm === true);
     });
 };
-$scope.showTabDialog = function(ev) {
-    $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'tabDialog.tmpl.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose:true
-    })
-        .then(function(answer) {
-            $scope.status = 'You said the information was "' + answer + '".';
-        }, function() {
-            $scope.status = 'You cancelled the dialog.';
-        });
-};
+//$scope.showTabDialog = function(ev) {
+//    $mdDialog.show({
+//        controller: DialogController,
+//        templateUrl: 'tabDialog.tmpl.html',
+//        parent: angular.element(document.body),
+//        targetEvent: ev,
+//        clickOutsideToClose:true
+//    })
+//        .then(function(answer) {
+//            $scope.status = 'You said the information was "' + answer + '".';
+//        }, function() {
+//            $scope.status = 'You cancelled the dialog.';
+//        });
+//};
 }]);
 
 function DialogController($scope, $mdDialog) {
