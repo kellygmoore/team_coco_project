2cc0ea5ec22103c1c827f070371ec23581392da5
lahusiak <lahusiak@gmail.com>
2015-12-17 17:25:20 -0600

    ReserveBookScreen updated and styled 121715

--- server/public/assets/scripts/controllers/incrementcontroller.js
+++ server/public/assets/scripts/controllers/incrementcontroller.js
@@ -10,6 +10,7 @@ myApp.controller('IncrementCtrl', ['$scope', function($scope){
         people: 2,
         hours: 1,
         minutes: 0
+
     };
 
     $scope.peopleAdd = function(){
@@ -18,10 +19,20 @@ myApp.controller('IncrementCtrl', ['$scope', function($scope){
 
     $scope.peopleRemove= function(){
         $scope.meetingValues.people--;
+        if($scope.meetingValues.people - 1 <=0){
+            $scope.meetingValues.people = 0
+        } else {
+            $scope.meetingValues.people--;
+        }
     };
 
     $scope.hourRemove= function(){
         $scope.meetingValues.hours--;
+        if($scope.meetingValues.hours - 1 <=0){
+            $scope.meetingValues.hours = 0
+        } else {
+            $scope.meetingValues.hours--;
+        }
     };
 
     $scope.hourAdd= function(){
@@ -30,11 +41,20 @@ myApp.controller('IncrementCtrl', ['$scope', function($scope){
 
     $scope.minuteAdd= function (){
         $scope.meetingValues.minutes+=15;
+        if($scope.meetingValues.minutes + 15 > 45){
+            $scope.meetingValues.minutes = 45;
+        } else{$
+            scope.meetingValues.minutes+=15}
     };
 
     $scope.minuteRemove=function(){
         $scope.meetingValues.minutes-=15;
+        if($scope.meetingValues.minutes - 15 <=0){
+            $scope.meetingValues.minutes = 0
+        } else {
+            $scope.meetingValues.minutes-=15;
+        }
     };
-//}
+
 
 }]);
