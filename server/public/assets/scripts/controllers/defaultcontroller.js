50ce9ddc51e59156bc4d0dbf0732bff5c89ffaed
Sam Moss <sam.moss321@gmail.com>
2015-12-18 16:29:14 -0600

    end of week commit

--- server/public/assets/scripts/controllers/defaultcontroller.js
+++ server/public/assets/scripts/controllers/defaultcontroller.js
@@ -2,7 +2,7 @@
  * Created by samuelmoss on 12/17/15.
  */
 //Controller for DEFAULT screen view////////////////////////////////////////////////////
-myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "SharedBookedNameData", "$timeout", function($scope, $location, SharedRoomData, SharedBookedNameData, $timeout){
+myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "SharedBookedNameData", "$timeout", "$interval",function($scope, $location, SharedRoomData, SharedBookedNameData, $timeout, $interval){
 //change out to data from Bamboo
 
     //console.log("default controller works");
@@ -10,7 +10,7 @@ myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "Share
     $scope.timeLeftHr = 1;
     $scope.timeLeftMin = 36;
     $scope.nextMtgAt = "No Scheduled Meetings";      //string or number?
-    $scope.roomBooked = true;
+    $scope.roomBooked = undefined;
     $scope.roomData = SharedRoomData;
     $scope.bookingData = SharedBookedNameData;
     $scope.meetingTimesArray = [];
@@ -22,9 +22,9 @@ myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "Share
     //Conditional to check and see if room data had been pulled into the roomData Factory.
     //If not it calls a method in the factory which hits the API and gets all the
     //information for the room
-    if($scope.roomData === undefined){
+    if($scope.roomData.retrieveRoomData() === undefined){
         $scope.roomData.setRoomData();
-        $scope.roomData = $scope.roomData.retrieveRoomData();
+        $scope.roomData.retrieveRoomData();
     }
 
     $scope.roomName = $scope.roomData.name;
@@ -36,15 +36,17 @@ myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "Share
         $scope.bookingData.retrieveBambooData()
                 .then(function(){
                     $scope.bookedData = $scope.bookingData.setBambooData();
-                    console.log("promise meeting data: ", $scope.bookedData);
+                    //console.log("promise meeting data: ", $scope.bookedData);
                 $scope.updateMeetingTimesArray();
-                console.log("Meeting times array: ", $scope.meetingTimesArray);
+                //console.log("Meeting times array: ", $scope.meetingTimesArray);
+                $scope.meetingTimeSwitch();
                 });
     } else {
         $scope.bookedData = $scope.bookingData.setBambooData();
-        console.log("promise meeting data: ", $scope.bookedData);
+        //console.log("promise meeting data: ", $scope.bookedData);
         $scope.updateMeetingTimesArray();
-        console.log("Meeting times array: ", $scope.meetingTimesArray);
+        //console.log("Meeting times array: ", $scope.meetingTimesArray);
+        $scope.meetingTimeSwitch();
     }
 
     //Function which searches through all the meeting for the locations,
@@ -54,21 +56,22 @@ myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "Share
         $scope.bookedData.map(
             function(obj) {
                 if(obj.meetingRoom.id === 2/*$scope.roomData.id*/){
-                    var meetTimeObj = {};
-                    meetTimeObj.start = {};
-                    meetTimeObj.end = {};
+                    $scope.meetTimeObj = {};
+                    $scope.meetTimeObj.start = {};
+                    $scope.meetTimeObj.end = {};
                     var startTime = new Date(obj.startDate);
                     var endTime = new Date(obj.endDate);
                     //meetTimeObj.startTime = {};
                     //meetTimeObj.endTime = {};
-                    meetTimeObj.startTime = startTime.getTime();
-                    meetTimeObj.endTime = endTime.getTime();
-                    meetTimeObj.length = meetTimeObj.endTime - meetTimeObj.startTime;
-                    meetTimeObj.start.hour = parseInt(obj.startDate.slice(11, 13));
-                    meetTimeObj.start.minute = parseInt(obj.startDate.slice(14, 16));
-                    meetTimeObj.end.hour = parseInt(obj.endDate.slice(11, 13));
-                    meetTimeObj.end.minute = parseInt(obj.endDate.slice(14, 16));
-                    $scope.meetingTimesArray.push(meetTimeObj);
+                    $scope.meetTimeObj.startTime = startTime.getTime();
+                    $scope.meetTimeObj.endTime = endTime.getTime();
+                    $scope.meetTimeObj.elapse = $scope.meetTimeObj.endTime - $scope.meetTimeObj.startTime;
+                    $scope.meetTimeObj.start.hour = parseInt(obj.startDate.slice(11, 13));
+                    $scope.meetTimeObj.start.minute = parseInt(obj.startDate.slice(14, 16));
+                    $scope.meetTimeObj.end.hour = parseInt(obj.endDate.slice(11, 13));
+                    $scope.meetTimeObj.end.minute = parseInt(obj.endDate.slice(14, 16));
+                    $scope.meetingTimesArray.unshift($scope.meetTimeObj);
+                    console.log("meeting times array is reached");
                 }
             }
         );
@@ -81,32 +84,58 @@ myApp.controller('DefaultCtrl', ["$scope", "$location", "SharedRoomData", "Share
     //otherwise it sets itself to inactive mode.
     //This allows the script to be booted at any time and immediately update.
     $scope.meetingTimeSwitch = function(){
-        while($scope.bootTime > $scope.meetTimeObj[0].endTime){
-            $scope.meetTimeObj.shift();
+        while($scope.bootTime > $scope.meetingTimesArray[0].endTime){
+            $scope.meetingTimesArray.shift();
         }
-        if (($scope.bootTime > $scope.meetTimeObj[0].startTime) || ($scope.bootTime = $scope.meetTimeObj[0].startTime)){
+        console.log("Boot time: ", $scope.bootTime);
+        console.log("Next start time: ", $scope.meetingTimesArray[0].startTime);
+
+        if ($scope.bootTime > $scope.meetingTimesArray[0].startTime){
+            console.log("The if at switch is hit");
             $scope.activeMeetingLogic();
         } else {
+            console.log("The else at switch is hit");
             $scope.inActiveMeetingLogic();
         }
     };
 
     $scope.activeMeetingLogic = function(){
-        roombooked = true;
-        $scope.timeLeftHr= undefined;
-        $scope.timeLeftMin= undefined;
+        //console.log("active was reached");
+        //console.log("Here is the meeting array: ", $scope.meetingTimesArray);
+        $scope.roomBooked = true;
+        //console.log("Is the room booked?",$scope.roomBooked);
+        $scope.updateTime = function(){
+            var currentTime = new Date();
+            $scope.timeLeftHr = (($scope.meetingTimesArray[0].end.hour - currentTime.getHours()));
+            $scope.timeLeftMin = ($scope.meetingTimesArray[0].end.minute - currentTime.getMinutes());
+            if($scope.timeLeftMin < 0){
+                $scope.timeLeftMin+=60
+            }
+        };
+        $scope.updateTime();
+        $interval($scope.updateTime, 60000);
         $scope.meetingTimeout = $timeout(
-            $scope.inActiveMeetingLogic, ($scope.meetTimeObj[0].endTime - $scope.meetTimeObj[0].startTime)
+            $scope.inActiveMeetingLogic, ($scope.meetingTimesArray[0].endTime - $scope.meetingTimesArray[0].startTime)
         )
     };
 
     $scope.inActiveMeetingLogic = function(){
-        $scope.currentTime = new Date.now();
-        roombooked = false;
-        $scope.nextMtgAt = ""+$scope.meetTimeObj[0].end.hour+":"+$scope.meetTimeObj[0].end.minute+"";
+        //console.log("Here is the meeting array: ", $scope.meetingTimesArray);
+        //console.log("inactive was reached");
+        $scope.currentTime = Date.now();
+        $scope.roomBooked = false;
+        $scope.nextMtgAt = "" + ($scope.meetingTimesArray[0].start.hour)%12 + ":" + $scope.meetingTimesArray[0].start.minute +"";
         $scope.meetingTimeout = $timeout(
-            $scope.inActiveMeetingLogic, ($scope.meetTimeObj[0].startTime - $scope.currentTime)
+            $scope.inActiveMeetingLogic, ($scope.meetingTimesArray[0].startTime - $scope.currentTime)
         )
+    };
+
+    $scope.adjustTimeLength = function(){
+        $scope.rawTime = "" + $scope.meetingTimesArray[0].end.minute + "";
+        if($scope.rawTime.length < 2){
+            $scope.rawTime = "0" + $scope.rawTime;
+        }
+        return $scope.rawTime;
     };
 
     //$scope.timeCheck = $scope.compareTime(Date.prototype.getHours(), Date.prototype.getMinutes())) ? $scope.roomBooked = true : $scope.roomBooked = false;
