cb2d11f265419a7641d7377aeef120247386e506
Sam Moss <sam.moss321@gmail.com>
2016-01-04 13:59:07 -0600

    start and end time functional

--- server/public/assets/scripts/controllers/ReserveBookCtrl.js
+++ server/public/assets/scripts/controllers/ReserveBookCtrl.js
@@ -2,7 +2,7 @@
  * Created by samuelmoss on 1/4/16.
  */
 
-myApp.controller('ReserveBookCtrl',['$scope', 'SharedTimeData', 'SharedBookedNameData', function($scope, SharedTimeData, SharedBookedNameData){
+myApp.controller('ReserveBookCtrl',['$scope', 'SharedTimeData', 'SharedBookedNameData', 'dateFilter', function($scope, SharedTimeData, SharedBookedNameData, dateFilter){
     //console.log("we are in the RBC");
 //RESERVEBOOK SCREEN
     //SharedTimeData is a factory that holds start time selected with ng-click by the user on the calendar view
@@ -10,7 +10,8 @@ myApp.controller('ReserveBookCtrl',['$scope', 'SharedTimeData', 'SharedBookedNam
     $scope.sharedTimeData = SharedTimeData;
     $scope.sharedBookedName = SharedBookedNameData;
     $scope.meetingTimesArray = undefined;
-
+    var startTime = {};
+    var endTime = {};
 
     $scope.updateMeetingTimeData = function(){
         console.log("step 2");
@@ -24,27 +25,48 @@ myApp.controller('ReserveBookCtrl',['$scope', 'SharedTimeData', 'SharedBookedNam
         );
     };
 
+    var startTimeSet = function() {
+        var currentHour = parseInt(dateFilter(Date.now(), 'hh'));
+        var currentMinute = parseInt(dateFilter(Date.now(), 'mm'));
+        switch (true) {
+            case (currentMinute > 0 && currentMinute < 15):
+                currentMinute = 15;
+                break;
+            case(currentMinute >= 15 && currentMinute < 30):
+                currentMinute = 30;
+                break;
+            case(currentMinute >= 30 && currentMinute < 45):
+                currentMinute = 45;
+                break;
+            case(currentMinute >= 45):
+                currentMinute = 0;
+                currentHour++;
+                break;
+        }
+        startTime.hour = currentHour;
+        startTime.minute = currentMinute;
+    }
+
+    startTimeSet();
+
+    var endTimeSet = function(){
+        endTime.minute = 0;
+        if(localStorage.selectLocation == 129){
+            endTime.hour = 6
+        } else {
+            endTime.hour = 5;
+        };
+    };
+
+    endTimeSet();
+
+    console.log("start time: ", startTime);
+    console.log("end time: ", endTime);
+
 
     if ($scope.meetingTimesArray == undefined) {
         $scope.updateMeetingTimeData();
     }
-
-
-    //Starts the script.
-
-
-    //if($scope.sharedBookedName.setBambooData() === null){
-    //    $scope.sharedBookedName.retrieveBambooData()
-    //        .then(function(){
-    //            $scope.meetingTimesArray = $scope.sharedBookedName.setBambooData();
-    //            console.log("(if) here is the data here: ", $scope.meetingTimesArray);
-    //        }
-    //    )
-    //} else {
-    //    $scope.meetingTimesArray = $scope.sharedBookedName.setBambooData();
-    //    console.log("(else) here is the data here: ", $scope.meetingTimesArray)
-    //}
-
 
 
     //$scope.tapToBook=function(hour){
@@ -54,6 +76,28 @@ myApp.controller('ReserveBookCtrl',['$scope', 'SharedTimeData', 'SharedBookedNam
 
     //startHour variable holds start time of meeting
     $scope.startHour = $scope.sharedTimeData.retrieveTimeData();
+
+    $scope.constructTimeObject = function(time){
+        //This function is going to be called every time there is a start time without a meeting in
+        //session and is going to construct and object consisting of a key of start time
+        //then a value of the hour and the minutes.
+    };
+
+    $scope.constructTimeArray = function(meetingTimes){
+        $scope.data.availableStartTime = [];
+
+        for(var i = startTime;i < endTime; i++){
+            $scope.constructTimeObject();
+        }
+        //We'll need to know: what time it is, how long until the building closes
+        //and also all the times where meetings are being held.
+        //Then we are going to construct a start and end bound time and
+        //create a time object for each of these, skipping over all the times
+        //where a meeting is in session.
+        //Return some array that we will set 'availableStartTime' to.
+    };
+
+    //$scope.constructTimeArray(meetingTimesArray);
 
     //The following populates the dropdown menus on the reserveBookScreen
     $scope.data = {
